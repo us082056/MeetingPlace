0b57585b6c553b04c414215fa2b80769dd6e92a1
masayuki <masayuki@192.168.0.3>
2016-02-19 00:02:54 +0900

    GET化

--- app/controllers/SearchController.scala
+++ app/controllers/SearchController.scala
@@ -21,35 +21,33 @@ class SearchController extends Controller {
     Ok(views.html.index())
   }
 
-  def search = Action {
-    req =>
-      {
-        //TODO バリデーション
+  //requestをimplicitで宣言することで、暗黙的にリクエストを引数として受け取る
+  def search = Action { implicit request =>
+    //TODO バリデーション
 
-        //駅名取得（入力値取得）・・・（仮） 
-        val s1 = req.body.asFormUrlEncoded.get("station1")(0).toString()
-        val s2 = req.body.asFormUrlEncoded.get("station2")(0).toString()
-        var inputStations: List[Station] = List(new Station(s1, "", null), new Station(s2, "", null))
-        //TODO 画面から入力値を取得し、リストに詰める
+    //駅名取得（入力値取得）・・・（仮） 
+    val s1 = Form("station1" -> text).bindFromRequest().get
+    val s2 = Form("station2" -> text).bindFromRequest().get
+    var inputStations: List[Station] = List(new Station(s1, "", null), new Station(s2, "", null))
+    //TODO 画面から入力値を取得し、リストに詰める
 
-        //経緯度取得
-        for (station <- inputStations) {
-          //駅名を駅コードに変換する
-          station.code = StationsManager.nameToCode(station.name)
+    //経緯度取得
+    for (station <- inputStations) {
+      //駅名を駅コードに変換する
+      station.code = StationsManager.nameToCode(station.name)
 
-          //webAPIで駅コードの経緯度取得する
-          station.lonLat = StationsManager.codeToLonLat(station.code)
-        }
+      //webAPIで駅コードの経緯度取得する
+      station.lonLat = StationsManager.codeToLonLat(station.code)
+    }
 
-        //中心経緯度計算
-        val centerLonLat = new LonLatCalculator().calcCenterLonLat(inputStations)
+    //中心経緯度計算
+    val centerLonLat = new LonLatCalculator().calcCenterLonLat(inputStations)
 
-        //最寄駅取得
-        val nearStationsName = StationsManager.getNearStationsName(centerLonLat)
+    //最寄駅取得
+    val nearStationsName = StationsManager.getNearStationsName(centerLonLat)
 
-        //出力
-        //TODO 画面出力する(仮）
-        Ok(views.html.result(nearStationsName(0)))
-      }
+    //出力
+    //TODO 画面出力する(仮）
+    Ok(views.html.result(nearStationsName(0)))
   }
 }
