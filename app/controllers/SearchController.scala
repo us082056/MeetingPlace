695616e3181debf68b1147188867bfc08f5f42ea
masayuki <masayuki@192.168.0.3>
2016-02-18 19:12:05 +0900

    とりあえず動くようにした

--- app/controllers/SearchController.scala
+++ app/controllers/SearchController.scala
@@ -8,8 +8,9 @@ import play.api.mvc.Action
 import play.api.mvc.Controller
 import servicies.LonLatCalculator
 import util.StationsManager
-import play.api.Play.current //・・・TODO　おまじない
+import play.api.Play.current
 import play.api.i18n.Messages.Implicits._ //・・・TODO おまじない
+import models.Station
 
 class SearchController extends Controller {
   val stationForm = Form(
@@ -17,34 +18,38 @@ class SearchController extends Controller {
 
   def init = Action {
     val inputForm = stationForm.fill(StationForm("user name"))
-    Ok(views.html.index(inputForm))
+    Ok(views.html.index())
   }
 
   def search = Action {
-    //TODO バリデーション
+    req =>
+      {
+        //TODO バリデーション
 
-    //駅名取得（入力値取得）
-    var inputStations: List[Station] = null //・・・（仮） 
-    //TODO 画面から入力値を取得し、リストに詰める
+        //駅名取得（入力値取得）・・・（仮） 
+        val s1 = req.body.asFormUrlEncoded.get("station1")(0).toString()
+        val s2 = req.body.asFormUrlEncoded.get("station2")(0).toString()
+        var inputStations: List[Station] = List(new Station(s1, "", null), new Station(s2, "", null))
+        //TODO 画面から入力値を取得し、リストに詰める
 
-    //経緯度取得
-    for (station <- inputStations) {
-      //駅名を駅コードに変換する
-      station.code = StationsManager.nameToCode(station.name)
+        //経緯度取得
+        for (station <- inputStations) {
+          //駅名を駅コードに変換する
+          station.code = StationsManager.nameToCode(station.name)
 
-      //webAPIで駅コードの経緯度取得する
-      station.lonLat = StationsManager.codeToLonLat(station.code)
-    }
+          //webAPIで駅コードの経緯度取得する
+          station.lonLat = StationsManager.codeToLonLat(station.code)
+        }
 
-    //中心経緯度計算
-    val centerLonLat = new LonLatCalculator().calcCenterLonLat(inputStations)
+        //中心経緯度計算
+        val centerLonLat = new LonLatCalculator().calcCenterLonLat(inputStations)
 
-    //最寄駅取得
-    val nearStationsName = StationsManager.getNearStationsName(centerLonLat)
+        //最寄駅取得
+        val nearStationsName = StationsManager.getNearStationsName(centerLonLat)
 
-    //出力
-    //TODO 画面出力する(仮）
-    val inputForm = stationForm.fill(StationForm("user name"))
-    Ok(views.html.index(inputForm))
+        //出力
+        //TODO 画面出力する(仮）
+        Ok(views.html.result(nearStationsName(0)))
+      }
   }
 }
