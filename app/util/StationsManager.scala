d5a1298a7c51c2213c1f45416ecbb294f5b40fc3
hoge <hoge@hoge>
2016-03-01 15:51:14 +0900

    ・リファクタリング ・デバッグ文追加

--- app/util/StationsManager.scala
+++ app/util/StationsManager.scala
@@ -85,6 +85,8 @@ object StationsManager {
       new String((xml \ "name").text.getBytes("iso-8859-1"), "utf-8")
     }
 
+    seq.foreach { x => Logger.debug(x) }
+
     if (seq.size == 0) {
       return "中間地点近くに駅がありません"
     } else {
